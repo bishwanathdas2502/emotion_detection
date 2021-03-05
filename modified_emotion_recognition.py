f08aea0f981a5e2c094b7e045c59d5fb97e94d69
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2021-03-05 21:15:12 +0530

    reinserted all code
    the file is created again

--- modified_emotion_recognition.py
+++ modified_emotion_recognition.py
@@ -1,24 +1,27 @@
-import sys, os
-import pandas as pd
+import math
 import numpy as np
-from keras.losses import categorical_crossentropy
-from keras.optimizers import Adam
-from keras.regularizers import l2
+import pandas as pd
+from matplotlib import pyplot
+import tensorflow as tf
+from tensorflow.keras import optimizers
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dropout, BatchNormalization, LeakyReLU, Activation
+from tensorflow.keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.optimizers import SGD
-import cv2
+
+
+df = pd.read_csv('fer2013.csv')
 
 
 
-df=pd.read_csv('fer2013.csv')
+INTERESTED_LABELS = [3, 4, 6]
 
-# print(df.info())
-# print(df["Usage"].value_counts())
+df = df[df.emotion.isin(INTERESTED_LABELS)]
 
-# print(df.head())
 X_train,train_y,X_test,test_y=[],[],[],[]
 
 for index, row in df.iterrows():
@@ -34,13 +37,7 @@ for index, row in df.iterrows():
         print(f"error occured at index :{index} and row:{row}")
 
 
-num_features = 64
 num_labels = 7
-batch_size = 64
-epochs = 20
-width, height = 48, 48
-
-
 X_train = np.array(X_train,'float32')
 train_y = np.array(train_y,'float32')
 X_test = np.array(X_test,'float32')
@@ -51,78 +48,196 @@ test_y=np_utils.to_categorical(test_y, num_classes=num_labels)
 
 #cannot produce
 #normalizing data between oand 1
-X_train -= np.mean(X_train, axis=0)
-X_train /= np.std(X_train, axis=0)
-
-X_test -= np.mean(X_test, axis=0)
-X_test /= np.std(X_test, axis=0)
+X_train = X_train / 255.
+X_test = X_test / 255.
 
 X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
 
 X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
 
-# print(f"shape:{X_train.shape}")
-##designing the cnn
-#1st convolution layer
-model = Sequential()
-model.add(ZeroPadding2D((1,1),input_shape=(48,48,1)))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2),padding='same', strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2),padding='same', strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2),padding='same', strides=(2,2)))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2),padding='same', strides=(2,2)))
+img_width = X_train.shape[1]
+img_height = X_train.shape[2]
+img_depth = X_train.shape[3]
+num_classes = train_y.shape[1]
 
 
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2),padding='same', strides=(2,2)))
+def build_net(optim):
+    net = Sequential(name='DCNN')
 
-model.add(Flatten())
-model.add(Dense(4096, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(4096, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7, activation='softmax'))
-# model.summary()
+    net.add(
+        Conv2D(
+            filters=64,
+            kernel_size=(5, 5),
+            input_shape=(img_width, img_height, img_depth),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_1'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_1'))
+    net.add(
+        Conv2D(
+            filters=64,
+            kernel_size=(5, 5),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_2'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_2'))
 
-#Compliling the model
-model.compile(loss=categorical_crossentropy,
-              optimizer=Adam(),
-              metrics=['accuracy'])
+    net.add(MaxPooling2D(pool_size=(2, 2), name='maxpool2d_1'))
+    net.add(Dropout(0.4, name='dropout_1'))
 
-#Training the model
-model.fit(X_train, train_y,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(X_test, test_y),
-          shuffle=True)
+    net.add(
+        Conv2D(
+            filters=128,
+            kernel_size=(3, 3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_3'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_3'))
+    net.add(
+        Conv2D(
+            filters=128,
+            kernel_size=(3, 3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_4'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_4'))
 
+    net.add(MaxPooling2D(pool_size=(2, 2), name='maxpool2d_2'))
+    net.add(Dropout(0.4, name='dropout_2'))
+
+    net.add(
+        Conv2D(
+            filters=256,
+            kernel_size=(3, 3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_5'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_5'))
+    net.add(
+        Conv2D(
+            filters=256,
+            kernel_size=(3, 3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_6'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_6'))
+
+    net.add(MaxPooling2D(pool_size=(2, 2), name='maxpool2d_3'))
+    net.add(Dropout(0.5, name='dropout_3'))
+
+    net.add(Flatten(name='flatten'))
+
+    net.add(
+        Dense(
+            128,
+            activation='elu',
+            kernel_initializer='he_normal',
+            name='dense_1'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_7'))
+
+    net.add(Dropout(0.6, name='dropout_4'))
+
+    net.add(
+        Dense(
+            num_classes,
+            activation='softmax',
+            name='out_layer'
+        )
+    )
+
+    net.compile(
+        loss='categorical_crossentropy',
+        optimizer=optim,
+        metrics=['accuracy']
+    )
+
+
+
+    return net
+
+early_stopping = EarlyStopping(
+    monitor='val_accuracy',
+    min_delta=0.00005,
+    patience=11,
+    verbose=1,
+    restore_best_weights=True,
+)
+
+lr_scheduler = ReduceLROnPlateau(
+    monitor='val_accuracy',
+    factor=0.5,
+    patience=7,
+    min_lr=1e-7,
+    verbose=1,
+)
+
+callbacks = [
+    early_stopping,
+    lr_scheduler,
+]
+
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.15,
+    height_shift_range=0.15,
+    shear_range=0.15,
+    zoom_range=0.15,
+    horizontal_flip=True,
+)
+
+train_datagen.fit(X_train)
+
+
+
+optims = [
+    optimizers.Nadam(
+        learning_rate=0.001,
+        beta_1=0.9,
+        beta_2=0.999,
+        epsilon=1e-07,
+        name='Nadam'),
+
+    optimizers.Adam(0.001),
+]
+
+num_features = 64
+num_labels = 7
+batch_size = 32
+epochs = 100
+width, height = 48, 48
+
+
+model = build_net(optims[1])
+
+model.fit(
+    train_datagen.flow(X_train, train_y, batch_size=batch_size),
+    validation_data=(X_test, test_y),
+    steps_per_epoch=len(X_train) / batch_size,
+    epochs=epochs,
+    callbacks=callbacks,
+
+)
 
 #Saving the  model to  use it later on
 fer_json = model.to_json()
